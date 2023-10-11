0041d429e7d514d011e41714cb703d31d77d05c8
grazy <124395628+vgrazysz@users.noreply.github.com>
2023-10-11 07:31:33 -0300

    Atualizar o q18.py

--- q18.py
+++ q18.py
@@ -1,8 +1,9 @@
 nome = input("Informe o nome:")
-sexo = input("Informe o sexo (M para masculino, F para feminino ou X para outro):")
-estado_civil = input("Digite o estado civil (solteira, casada, viúva, namorando): ")
-if sexo.upper() == "F" and estado_civil.upper() == "casada":
-    tempo_casada = int(input("Informe o tempo de casamento em anos:"))
-    print(f"{nome} está casada há {tempo_casada} anos.")
+genero = input(f"Informe seu genero {nome} (M para masculino, F para feminino ou X para outro):")
+estado_civil = input(f"Digite o estado civil {nome} (solteiro, namorando, casado, ou viúvo): ")
+estado_civil = estado_civil.upper()
+genero = gênero.upper()
+if gênero == 'F' and estado_civil == 'CASADO' or estado_civil 'CASADA' 
+temp = input ("informe seu tempo de casada")
 else:
-    print(f"{nome} não está casada.")
+print ("Valeu!")
